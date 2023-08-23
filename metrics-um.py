e909abf8157cd96309df96de0bd23cf92c6d3874
ysdede <yunusseyhandede@gmail.com>
2023-08-23 19:27:50 +0300

    Add contract options.

--- metrics-um.py
+++ metrics-um.py
@@ -2,12 +2,12 @@ from BulkBase import BulkBase
 
 if __name__ == "__main__":
     binanceDownloader = BulkBase(
-        start="2021-11-01",
-        end="2023-08-18",
+        start="2022-11-01",
+        end="today",
         market_type="futures",
         margin_type="um",
         data_type="metrics",
-        symbol="BTC-USDT",
+        symbol="BTCUSDT",  # um: BTCUSDT   BTCUSDT_211231  BTCUSDT_220325  BTCUSDT_220624  BTCUSDT_220930  BTCUSDT_221230  BTCUSDT_230331  BTCUSDT_230630  BTCUSDT_230929  BTCUSDT_231229  
         worker_count=16,
         force_daily=True,
         merge=True,
