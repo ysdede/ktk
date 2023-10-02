e9032363e7766753b50caa6868fd91c088113412
ysdede <yunusseyhandede@gmail.com>
2023-10-02 14:27:17 +0300

    New BTCBUSD PERP

---
+++ klines-futures-BTCBUSD.py
@@ -0,0 +1,16 @@
+from BulkBase import BulkBase
+
+if __name__ == "__main__":
+    binanceDownloader = BulkBase(
+        start="2019-01-01",
+        end="today",
+        market_type="futures",
+        worker_count=10,
+        data_type="klines",
+        symbol="BTC-BUSD",
+        tf="1h",
+        force_daily=False,
+        merge=True,
+    )
+
+    binanceDownloader.run()
